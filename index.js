716426cb63df5ad153c83fe4802f129291dc4937
Nicole Gomez <nicolegomezx3@gmail.com>
2023-03-08 12:27:23 -0500

    Adds codes for destructive and nondestructive functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...employee, [key]: value};
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const copyOfObj = {...obj};
+    delete copyOfObj[key];
+    return copyOfObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
